a36b4d03c048f240e2fd56a8a7cf2473ff13eb89
JoseQA81 <161597752+JoseQA81@users.noreply.github.com>
2024-05-15 23:25:07 +0200

    IOT-2  See the answer
    Falta algún detalle

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function mostrarMensaje() {
     document.getElementById("us1Welcome").innerText = mensaje;
     
 }
+
+//USER STORY - SEE THE ANSWER
+function showAnswer() {
+    let answer = "C";
+
+    let message = "The correct answer is " + answer;
+    document.getElementById("us2SeeAnswer").innerText = message;
+}
