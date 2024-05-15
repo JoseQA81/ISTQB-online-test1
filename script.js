1b264e3988a2df5eae2d98db7422e562898bedd2
JoseQA81 <161597752+JoseQA81@users.noreply.github.com>
2024-05-15 16:40:51 +0200

    IOT-1 Welcome the student message
    https://bootcampqajose.atlassian.net/browse/IOT-1

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-document.write("HOLA MUNDO");
+//USER STORY - WELCOME MESSAGE
+function mostrarMensaje() {
+    let nombre = document.getElementById("josWelcome").value;
+
+    let mensaje = "Welcome " + nombre + " to your ISTQB exam, " + " good luck! ";
+    document.getElementById("us1Welcome").innerText = mensaje;
+    
+}
+
