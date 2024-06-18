674bdd6ad3e05c12d9e9d8b125e53395044964f8
JoseQA81 <161597752+JoseQA81@users.noreply.github.com>
2024-06-18 11:48:13 +0200

    https://bootcampqajose.atlassian.net/browse/IOT-7 Ranking
    See ranking

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //USER STORY -  10 QUESTIONS - ARRAY WITH ALL CORRECT ANSWER
 const allCorrectAnswers = ["B", "A", "A", "C", "C", "B", "A", "C", "B", "B"];
 
+// ARRAY TO SAVE THE STUDENTS
+let students = [];
+
 //CREO UNA FUNCIÓN PARA IMPRIMIR
 function imprimir(mensaje, id) {
     let elemento = document.getElementById(id);
@@ -9,9 +12,9 @@ function imprimir(mensaje, id) {
 
 //USER STORY - WELCOME MESSAGE
 function mostrarMensaje() {
-    let nombre = document.getElementById("josWelcome").value;
+    let name = document.getElementById("josWelcome").value;
 
-    let mensaje = "Welcome " + nombre + " to your ISTQB exam, " + " good luck! ";
+    let mensaje = "Welcome " + name + " to your ISTQB exam, " + " good luck! ";
     imprimir(mensaje, "us1Welcome");
 
 }
@@ -75,6 +78,9 @@ function seeFinalScore() {
     mensaje = "Your final score is: " + finalScore + " points";
     imprimir(mensaje, "finalScore");
 
+    let studentName = document.getElementById("josWelcome").value;
+    addStudent(studentName, finalScore); //ADD STUDENT TO THE RANKING
+
     setTimeout(restartTest, 5000); // Wait a moment before restarting the test to allow the message to be displayed.
 }
 
@@ -86,4 +92,32 @@ function restartTest() {
         document.getElementById("us2SeeScore" + i).innerText = "";
     }
     document.getElementById("finalScore").innerText = "";
+}
+
+//USER STORY - RANKING
+function addStudent(name, score) {
+
+    let newStudent = {
+        name: name,
+        score: score
+    };
+
+    students.push(newStudent);
+}
+
+//La función sort() se utiliza para ordenar arrays
+//Si 'b.score' es mayor que 'a.score', la diferencia será positiva y 'b' aparecerá antes que 'a'
+function seeRanking() {
+    students.sort((a, b) => b.score - a.score);
+    let mensaje = "";
+
+    for (i = 0; i < students.length; i++) {
+        let student = students[i];
+
+        mensaje += "\n Student: " + (i + 1);
+        mensaje += "\n Name: " + student.name;
+        mensaje += "\n Marks: " + student.score;
+        mensaje += "\n\n";
+    }
+    imprimir(mensaje, "rankingResult");
 }
